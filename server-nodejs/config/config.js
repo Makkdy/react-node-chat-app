5a5842baf09e5f8487d4e64629dd8286e35d3975
Makkdy <mubbaseermakrod567@gmail.com>
2023-09-08 12:00:28 -0400

    first commit

---
+++ server-nodejs/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    NODE_PORT: process.env.NODE_PORT || "3001",
+    CHAT_ENGINE_PROJECT_ID: process.env.CHAT_ENGINE_PROJECT_ID || "04b533d6-d05a-4825-9d4a-ca32b8bb35a4",
+    CHAT_ENGINE_PRIVATE_KEY: process.env.CHAT_ENGINE_PRIVATE_KEY || "ea660016-5b3b-4603-9667-64f31f2eadd4",
+};
